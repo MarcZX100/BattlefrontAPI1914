ef6cf5d21eff0bba52344e88dcfc30c5d6e5231d
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2025-01-11 02:29:39 +0000

    Code style fixes

--- src/endpoints/Users.ts
+++ src/endpoints/Users.ts
@@ -14,8 +14,8 @@ export class UserApi {
 
   constructor(apiClient: Record<string, any>) {
     this.apiClient = apiClient;
-    this.defaultUserOptionsArray = ["username", "avatarURL", "regTstamp", "alliance", "rankProgress", "gameStats"]
-    this.allUserOptionsArray = ['acl', 'alliance', 'allianceInvites', 'allianceMemberShip', 'askForEmail', 'askForPassword', 'avatarURL', 'country', 'deletionStatus', 'email', 'emailChangeRequest', 'inventory', 'isPaying', 'battlePassProgress', 'lastOfferPurchaseTimeSeconds', 'links', 'minModVersion', 'notifications', 'pushNotificationPreferences', 'rank', 'rankProgress', 'referrer', 'regTstamp', 'shopPlatform', 'showSocialMediaButtons', 'isAllowedToShowStoreLinks', 'sources', 'subscriptions', 'unreadMessages', 'useFastPaypalCheckout', 'username', 'useShop2017', 'canAdjustEmail', 'shouldDisableInGameUserRegistration', 'canUseInventorySystem', 'publisherID', 'qualityMatchAdsSupport', 'useFirefly', 'mayUseGgsShopWithoutPaymentMethods','stats','scenarioStats','awardProgress', "gameStats"]
+    this.defaultUserOptionsArray = ["username", "avatarURL", "regTstamp", "alliance", "rankProgress", "gameStats"];
+    this.allUserOptionsArray = ["acl", "alliance", "allianceInvites", "allianceMemberShip", "askForEmail", "askForPassword", "avatarURL", "country", "deletionStatus", "email", "emailChangeRequest", "inventory", "isPaying", "battlePassProgress", "lastOfferPurchaseTimeSeconds", "links", "minModVersion", "notifications", "pushNotificationPreferences", "rank", "rankProgress", "referrer", "regTstamp", "shopPlatform", "showSocialMediaButtons", "isAllowedToShowStoreLinks", "sources", "subscriptions", "unreadMessages", "useFastPaypalCheckout", "username", "useShop2017", "canAdjustEmail", "shouldDisableInGameUserRegistration", "canUseInventorySystem", "publisherID", "qualityMatchAdsSupport", "useFirefly", "mayUseGgsShopWithoutPaymentMethods","stats","scenarioStats","awardProgress", "gameStats"];
     this.rankingOptionsArray = ["monthRank", "weekRank", "globalRank", "highestMonthRank", "highestWeekRank", "lastMonthRank", "lastWeekRank"];
   }
 
@@ -26,7 +26,7 @@ export class UserApi {
    * @returns User
    */
   async getDetails(userID: number, options: string[] = this.defaultUserOptionsArray): Promise<any> {
-    let startTime = Date.now();
+    const startTime = Date.now();
 
     const data: Record<string, any> = { userID };
 
@@ -40,59 +40,59 @@ export class UserApi {
       });
     }
 
-    let result = await this.apiClient.sendRequest('getUserDetails', data);
-    result.elapsedTime = (Date.now() - startTime)
+    const result = await this.apiClient.sendRequest("getUserDetails", data);
+    result.elapsedTime = (Date.now() - startTime);
     return result;
   }
 
   async search(username: string, exactResult: boolean = false) {
-    let startTime = Date.now();
+    const startTime = Date.now();
 
-    let data = {
+    const data = {
       username: username
     };
 
-    let result = await this.apiClient.sendRequest('searchUser', data);
+    const result = await this.apiClient.sendRequest("searchUser", data);
     if (exactResult) {
       if ((result.result).find((x: UserSearch) => (x.username).toLowerCase() == username.toLowerCase())) {
-        result.result = [(result.result).find((x: UserSearch) => (x.username).toLowerCase() == username.toLowerCase())]
+        result.result = [(result.result).find((x: UserSearch) => (x.username).toLowerCase() == username.toLowerCase())];
       } else {
-        result.resultCode = -1
-        result.resultMessage = "not found"
+        result.resultCode = -1;
+        result.resultMessage = "not found";
       }
     }
-    result.elapsedTime = (Date.now() - startTime)
+    result.elapsedTime = (Date.now() - startTime);
     return result;
   }
 
   async sendMail(targetUserID: number, subject: string, body: string) {
-    let startTime = Date.now();
+    const startTime = Date.now();
     
-    let data = {
+    const data = {
       receiverID: targetUserID,
       subject: subject,
       body: body,
       mode: "pm"
-    }
+    };
 
-    let result = await this.apiClient.sendRequest('sendMessage', data);
-    result.elapsedTime = (Date.now() - startTime)
+    const result = await this.apiClient.sendRequest("sendMessage", data);
+    result.elapsedTime = (Date.now() - startTime);
     return result;
   }
 
   async getRanking(type: string = "globalRank", page: number = 0, numEntries: number = 10) {
-    let startTime = Date.now();
+    const startTime = Date.now();
 
-    let data = {
+    const data = {
       type: type, // ["monthRank", "weekRank", "globalRank", "highestMonthRank", "highestWeekRank", "lastMonthRank", "lastWeekRank"]
       page: page,
       numEntries: numEntries
     };
 
     if (numEntries > 50){
-      console.warn("The maximum number of entries allowed is 50.")
+      console.warn("The maximum number of entries allowed is 50.");
     } else if (numEntries < 5) {
-      console.warn("The minimum number of entries allowed is 5.")
+      console.warn("The minimum number of entries allowed is 5.");
     };
 
     if (!(this.rankingOptionsArray).includes(type)) {
@@ -102,11 +102,11 @@ export class UserApi {
         result: `The ${type} type does not exist.`,
         elapsedTime: (Date.now() - startTime),
         receivedData: data
-      }
+      };
     };
 
-    let result = await this.apiClient.sendRequest('getRankingFirefly', data);
-    result.elapsedTime = (Date.now() - startTime)
+    const result = await this.apiClient.sendRequest("getRankingFirefly", data);
+    result.elapsedTime = (Date.now() - startTime);
     return result;
   }
 }
