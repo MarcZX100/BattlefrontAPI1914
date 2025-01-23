568c837048c7d67254d2e21d1addf746cc05492a
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2025-01-23 14:30:47 +0000

    Released version 1.1.1

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,47 @@
-import { BytroFront } from 'bytrofront';
+const { BytroFront } = require("../dist/index.js");
+// const config = require("./sup.config.js");
 
-(async () => {
+// let client = new BytroFront(config);
 
-    // Generate game config. Accessible at hup.config in the client
-    let config = await BytroFront.generateConfig("MarcZX10", "realPassword11", "supremacy1914.com");
+// client.Users.sendMail(48035824, "hola", "test API").then((response) => {
+//   console.log(response);
+// });
 
-    const client = new BytroFront(config);
+// client.Users.getRanking("monthRank", 0, 6).then((response) => {
+//   console.log(response);
+// });
 
-    // Fetch user details
-    client.Users.getDetails(48035824).then((response) => {
-        console.log(response);
-    });
 
-    // Fetch first 10 players in the global ranking
-    client.Users.getRanking("globalRank", 0, 10).then((response) => {
-        console.log(response);
-    });
+// client.Games.getNewspaper(9551352, 2).then(result=>{
+//   console.log(result)
+// })
 
-})
+// console.log(client.Util.contentItems)
+// client.Util.loadContentItems().then(result=>{
+//   console.log(Object.keys(client.Util.contentItems))
+// })
+
+// client.Games.getOverviewOld(9536344).then(map=>{
+//   console.log((map.result.logins).map(x=>parseInt(x.siteUserID)))
+// })
+
+
+// client.Users.getDetails(48035824).then((response) => {
+//   console.log(response);
+// });
+
+async function test() {
+  console.log("Hola")
+  config = undefined;
+  while (!config) {
+    console.log("Voy a entrar a testear")
+    config = await BytroFront.generateConfig("USERNAME", "PASSWORD", "supremacy1914.com");
+    console.log("Salgo de testear")
+    console.log(config)
+    let client = new BytroFront(config);
+
+  }
+
+}
+
+test()
